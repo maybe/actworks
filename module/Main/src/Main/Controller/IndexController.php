db4fb22e0fa1b5c53c05820e0ef161cf2c1b7376
panda <panda@zoo.local>
2012-10-15 17:26:27 +0800

    first commit

---
+++ module/Main/src/Main/Controller/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Main IndexController
+ * @copyright Copyright (c) 2005-2012 BadPanda Inc.
+ */
+
+namespace Main\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $actionName = $this->params('action');
+        $langName = $this->params('lang');
+        $controllerName = $this->params('controller');
+        
+        return array(
+            'lang'  => $langName,
+            'action' =>$actionName,
+            'controller'  =>  $controllerName,
+        );
+    }
+    public function showAction()
+    {
+        $actionName = $this->params('action');
+        $langName = $this->params('lang');
+        $controllerName = $this->params('controller');
+        
+        return array(
+            'lang'  => $langName,
+            'action' =>$actionName,
+            'controller'  =>  $controllerName,
+        );
+    }
+}
